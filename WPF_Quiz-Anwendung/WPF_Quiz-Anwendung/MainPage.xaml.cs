f840c45eac4f70cea641f05e9254e08c114330ba
Janick Wahl <Janick.Wahl@htlwy.at>
2025-11-17 16:06:16 +0100

    added code-behind for QuestionPage & MainPage

--- WPF_Quiz-Anwendung/WPF_Quiz-Anwendung/MainPage.xaml.cs
+++ WPF_Quiz-Anwendung/WPF_Quiz-Anwendung/MainPage.xaml.cs
@@ -12,12 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF_Quiz_Anwendung.Classes;
 
 namespace WPF_Quiz_Anwendung
 {
-    /// <summary>
-    /// Interaktionslogik für MainPage.xaml
-    /// </summary>
     public partial class MainPage : Page
     {
         public MainPage()
@@ -28,7 +26,8 @@ namespace WPF_Quiz_Anwendung
 
         private void LoadQuiz(object sender, RoutedEventArgs e)
         {
-            
+            Quiz loadedQuiz = QuizFileHandler.LoadQuizFromFile();
+            NavigationService.Navigate(new QuestionPage());
         }
 
         private void OpenDefaultQuiz(object sender, RoutedEventArgs e)
@@ -38,7 +37,34 @@ namespace WPF_Quiz_Anwendung
 
         private void Navigate_CreateQuizPage(object sender, RoutedEventArgs e)
         {
-
+            var quiz = new WPF_Quiz_Anwendung.Classes.Quiz(
+                "Allgemeinwissen",
+                new List<WPF_Quiz_Anwendung.Classes.Question>
+                {
+                new WPF_Quiz_Anwendung.Classes.Question(
+                "Was ist die Hauptstadt von Deutschland?",
+                    WPF_Quiz_Anwendung.Classes.QuestionType.SingleRightAnswer,
+                    new List<WPF_Quiz_Anwendung.Classes.Answer>
+                    {
+                        new WPF_Quiz_Anwendung.Classes.Answer("Berlin", true),
+                        new WPF_Quiz_Anwendung.Classes.Answer("München", false),
+                        new WPF_Quiz_Anwendung.Classes.Answer("Hamburg", false)
+                    }
+                ),
+                new WPF_Quiz_Anwendung.Classes.Question(
+                    "Welche Farben hat die deutsche Flagge?",
+                    WPF_Quiz_Anwendung.Classes.QuestionType.MultipleRightAnswers,
+                    new List<WPF_Quiz_Anwendung.Classes.Answer>
+                    {
+                        new WPF_Quiz_Anwendung.Classes.Answer("Schwarz", true),
+                        new WPF_Quiz_Anwendung.Classes.Answer("Rot", true),
+                        new WPF_Quiz_Anwendung.Classes.Answer("Gelb", true),
+                        new WPF_Quiz_Anwendung.Classes.Answer("Grün", false)
+                    }
+                )
+                }
+            );
+            QuizFileHandler.SaveQuizToFile(quiz);
         }
 
         private void Navigate_SettingsPage(object sender, RoutedEventArgs e)
--- WPF_Quiz-Anwendung/WPF_Quiz-Anwendung/QuestionPage.xaml.cs
+++ WPF_Quiz-Anwendung/WPF_Quiz-Anwendung/QuestionPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF_Quiz_Anwendung.Classes;
 
 namespace WPF_Quiz_Anwendung
 {
@@ -20,9 +22,18 @@ namespace WPF_Quiz_Anwendung
     /// </summary>
     public partial class QuestionPage : Page
     {
-        public QuestionPage()
+        public QuestionPage(Quiz currentQuiz)
         {
             InitializeComponent();
+            for(int i = 0; i < currentQuiz.Questions.Count; i++)
+            {
+                LoadQuestion(currentQuiz.Questions[i]);
+            }
+            
+        }
+        public void LoadQuestion(Question currentQuestion)
+        {
+            currentQuestion.Answers.ForEach(answer => { AnswerGrid.Children.Add(new Button() { Content = answer.Title() }) });
         }
     }
 }
